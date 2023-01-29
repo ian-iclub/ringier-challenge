e67cd7d05dc00f9f155d3f0c5beda17388e7b6b3
Ian Irungu <ian.irungu@revolution-analytics.co.ke>
2023-01-29 21:57:10 +0300

    refactore(ui): apply changes for displaying images for show component

--- resources/js/Pages/Products/Show.vue
+++ resources/js/Pages/Products/Show.vue
@@ -11,6 +11,10 @@
             type: Object
         },
     });
+
+    function getBackgroundImage(product) {
+        return product.media[0]?.original_url ?? product.category.image
+    }
 </script>
 
 <template>
@@ -18,7 +22,7 @@
         <div class="container mx-auto px-6">
             <div class="md:flex md:items-center">
                 <div class="w-full h-64 md:w-1/2 lg:h-96">
-                    <img class="h-full w-full rounded-md object-cover max-w-lg mx-auto" :src="product.category.image" alt="Nike Air">
+                    <img class="h-full w-full rounded-md object-cover max-w-lg mx-auto" :src="getBackgroundImage(product)" alt="Nike Air">
                 </div>
                 <div class="w-full max-w-lg mx-auto mt-5 md:ml-8 md:mt-0 md:w-1/2">
                     <h3 class="text-gray-700 normal-case font-bold text-lg">{{ product.title }}</h3>
@@ -75,11 +79,12 @@
                     </div>
                 </div>
             </div>
+
             <div class="mt-16">
                 <h3 class="text-gray-600 text-2xl font-medium">Similar products in the same category</h3>
                 <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
                     <div v-for="product in category_products" :key="product.id" class="w-full max-w-sm mx-auto rounded-md shadow-lg overflow-hidden">
-                        <ProductCard>
+                        <ProductCard :backurl='getBackgroundImage(product)'>
                             <template #single>
                                 <Link :href="route('products.show', product)">
                                     <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
